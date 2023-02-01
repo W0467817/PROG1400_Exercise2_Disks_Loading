3e65fc90c4e008c4845ebdaa093d28fb978b160b
W0467817 <brett.longpre@outlook.com>
2023-02-01 13:16:42 -0400

    Final

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,52 @@
+import static java.lang.Math.PI;
+
 public class Circle {
+    private double radius;
+    private String color;
+
+
+
+    // CONSTRUCTOR
+    public Circle() {
+        this.radius = 2;
+        this.color = "green";
+
+    }
+
+    // SECOND OVERLOADED CONSTRUCTOR
+    public Circle(double radius) {
+        this.radius = radius;
+        this.color = "green";
+    }
+
+    // THIRD OVERLOADED CONSTRUCTOR
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+
+
+    // RADIUS GETTER METHOD
+    public double getRadius() {
+        return radius;
+    }
+
+    // COLOR GETTER METHOD
+    public String getColor() {
+        return color;
+    }
+
+    // CALCULATE TOTAL AREA
+    public double getArea() {
+        double r = getRadius();
+        double result = PI * r * r;
+        return result;
+    }
+
+    // DESCRIPTION OF CIRCLE
+    public String toString() {
+        String result = "%s and %s".formatted(getRadius(), getColor());
+        return result;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Circle firstCircle = new Circle();
+
+        Circle secondCircle = new Circle(6.0);
+
+        Circle thirdCircle = new Circle(8.0, "blue");
+
+        System.out.println("Provided by the getter methods, the first circle's color and radius are: " + firstCircle.getColor() + " and " + firstCircle.getRadius());
+        System.out.println("The total area of the first circle is " + firstCircle.getArea());
+        System.out.println("Provided by the toString() method, the first circle's color and radius are: " + firstCircle.toString() + "\n");
+
+        System.out.println("Provided by the getter methods, the second circle's color and radius are: " + secondCircle.getColor() + " and " + secondCircle.getRadius());
+        System.out.println("The total area of the second circle is " + secondCircle.getArea());
+        System.out.println("Provided by the toString() method, the second circle's color and radius are: " + secondCircle.toString() + "\n");
+
+        System.out.println("Provided by the getter methods, the third circle's color and radius are: " + thirdCircle.getColor() + " and " + thirdCircle.getRadius());
+        System.out.println("The total area of the third circle is " + thirdCircle.getArea());
+        System.out.println("Provided by the toString() method, the third circle's color and radius are: " + thirdCircle.toString() + "\n");
     }
 }
